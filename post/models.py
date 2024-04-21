1475b5e21848bb68ec03ed0c9e97d970382285ff
motaz ahmed Rawi <motazalrawi2002@gmail.com>
2024-04-21 16:51:14 +0300

    add media

--- post/models.py
+++ post/models.py
@@ -3,19 +3,17 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title=models.CharField( max_length=50)
-    content=models.TextField(max_length=2000)
-    created_at=models.DateTimeField(auto_now_add=True,null=False)
-    updated_at = models.DateTimeField(auto_now=True, null=False)
-    models.ImageField(upload_to='post-img/')
-
-    
+    content = models.TextField(max_length=2000)
+    image= models.ImageField(upload_to='post-img/',null=True)
+    created_at = models.DateTimeField(auto_now_add=True,null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
 
     class Meta:
         verbose_name = ("post")
         verbose_name_plural = ("posts")
 
     def __str__(self):
-        return self.name
+        return self.title
 
     def get_absolute_url(self):
         return reverse("post_detail", kwargs={"pk": self.pk})
--- project/settings.py
+++ project/settings.py
@@ -118,6 +118,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+MEDIA_ROOT =  os.path.join(BASE_DIR,"media")   # <-- absolute path to the media directory on your server
+
+MEDIA_URL= '/media/'                          # <-- URL that handles
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
